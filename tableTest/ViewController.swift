99cbd0b1b3483015b4e76869c5238eae8a558865
sasakazu <sasakura.company@gmail.com>
2018-08-18 13:34:31 +0900

    first commit

---
+++ tableTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableTest
+//
+//  Created by 笹倉 一也 on 2018/08/15.
+//  Copyright © 2018年 sasakura.company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
